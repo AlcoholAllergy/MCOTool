b52080ce00d93c1fbdc29f81becf306b27c33434
AlcoholAllergy <a-_-@AlcoholAllergys-MacBook-Pro.local>
2023-11-15 22:48:39 -0500

    v.0.0.2 fixing

--- components/Sidebar/processingSystemOption/SystemItem/SystemItem.tsx
+++ components/Sidebar/processingSystemOption/SystemItem/SystemItem.tsx
@@ -49,6 +49,9 @@ const SystemItem = ({
   // Dispatch your Redux action to update the selected item
 
   const onClickHandler = () => {
+    if (disableList.includes(name)) {
+      return;
+    }
     dispatch(setSelectedSystem(currentSystem));
     dispatch(setCurrentSelectedSystem(name));
     dispatch(setLayerCount(layerCount));
@@ -61,12 +64,13 @@ const SystemItem = ({
     dispatch(setPromptsList([]));
   };
 
+  const disableList = ['Casper-Homeline', 'Casper-Residential'];
   // for css
   const itemClass = `mt-8 mb-8 mx-4 truncate select-none h-20 flex justify-center items-center text-l text-gray-300 hover:text-white px-3 py-2 ${
     name === currentSelectedSystem
       ? 'bg-blue-800 shadow-md border-4 border-gray-500 scale-95 rounded-lg p-2'
       : 'bg-gray-900 hover:bg-gray-600 transition duration-200 transform hover:scale-90 rounded-lg'
-  } min-w-[150px]`;
+  } min-w-[150px] `;
   return (
     <p className={`${itemClass} whitespace-normal`} onClick={onClickHandler}>
       {name}
