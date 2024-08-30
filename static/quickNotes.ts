b3c1877a8d1fb28af5dc4974114c6851950bd082
AlcoholAllergy <a-_-@AlcoholAllergys-MacBook-Pro.local>
2024-08-30 09:51:13 -0400

    quick note update

--- static/quickNotes.ts
+++ static/quickNotes.ts
@@ -5,7 +5,7 @@ const QUICK_NOTES_LIST = [
   },
   {
     id: 2,
-    value: 'Closing date updated. Revised files sent for registration.',
+    value: 'Files sent for registration.',
   },
   {
     id: 3,
@@ -13,19 +13,24 @@ const QUICK_NOTES_LIST = [
   },
   {
     id: 4,
-    value: 'ATTN CAC: liability exclusion reason updated. Please re-approve.',
+    value: 'Closing date updated. Revised files sent for registration.',
   },
   {
     id: 5,
+    value: 'ATTN CAC: liability exclusion reason updated. Please re-approve.',
+  },
+  {
+    id: 6,
     value:
       'ATTN CAC: closing date updated. Mortgage details re-pulled. Please re-approve.',
   },
   {
-    id: 6,
-    value: 'Files sent for registration.',
+    id: 7,
+    value:
+      'ATTN CAC: borrower changed, port indicator set to YES in order to generate new AC#. Please re-approve. Thank you',
   },
   {
-    id: 7,
+    id: 8,
     value:
       'Please note we cannot process this file as XXXX as the remaining term of the existing mortgage is less than 3 months. Please renew the rate or let the client has new rate and term.',
   },
