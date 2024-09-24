0fa61ced97227355f467ed3ead0350398b5bf3a2
AlcoholAllergy <a-_-@AlcoholAllergys-MacBook-Pro.local>
2024-09-24 09:48:29 -0400

    quick notes updated

--- static/quickNotes.ts
+++ static/quickNotes.ts
@@ -17,20 +17,24 @@ const QUICK_NOTES_LIST = [
   },
   {
     id: 5,
-    value: 'ATTN CAC: liability exclusion reason updated. Please re-approve.',
+    value: 'Layer is not a Telus member, files sent via RightFax.',
   },
   {
     id: 6,
-    value:
-      'ATTN CAC: closing date updated. Mortgage details re-pulled. Please re-approve.',
+    value: 'ATTN CAC: liability exclusion reason updated. Please re-approve.',
   },
   {
     id: 7,
     value:
-      'ATTN CAC: borrower changed, port indicator set to YES in order to generate new AC#. Please re-approve. Thank you',
+      'ATTN CAC: closing date updated. Mortgage details re-pulled. Please re-approve.',
   },
   {
     id: 8,
+    value:
+      'ATTN CAC: borrower changed, port indicator set to YES in order to generate new AC#. Please re-approve. Thank you',
+  },
+  {
+    id: 9,
     value:
       'Please note we cannot process this file as XXXX as the remaining term of the existing mortgage is less than 3 months. Please renew the rate or let the client has new rate and term.',
   },
